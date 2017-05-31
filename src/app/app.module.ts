460512c2237ae0117b717d0c70693c67593e4001
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2017-05-31 19:56:06 +0300

    [chore] FE: set html5 history routing mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -58,7 +58,7 @@ type StoreType = {
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(ROUTES, { useHash: true, preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(ROUTES, { useHash: false, preloadingStrategy: PreloadAllModules })
   ],
   /**
    * Expose our Services and Providers into Angular's dependency injection.
